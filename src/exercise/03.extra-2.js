392a989ac7f51b5f386458caf6fdde126145b844
Tuan Duong <quangtuan9237@gmail.com>
2022-01-12 23:46:02 +0700

    complete

--- src/exercise/03.extra-2.js
+++ src/exercise/03.extra-2.js
@@ -16,13 +16,29 @@ import {
 import {useAsync} from '../utils'
 
 // 🐨 Create a PokemonCacheContext
+const PokemonCacheContext = React.createContext()
 
 // 🐨 create a PokemonCacheProvider function
-// 🐨 useReducer with pokemonCacheReducer in your PokemonCacheProvider
-// 💰 you can grab the one that's in PokemonInfo
-// 🐨 return your context provider with the value assigned to what you get back from useReducer
-// 💰 value={[cache, dispatch]}
-// 💰 make sure you forward the props.children!
+function PokemonCacheProvider(props) {
+  const [cache, dispatch] = React.useReducer(pokemonCacheReducer, {})
+
+  return (
+    <PokemonCacheContext.Provider
+      value={[cache, dispatch]}
+      {...props}
+    ></PokemonCacheContext.Provider>
+  )
+}
+
+function usePokemonCache() {
+  const value = React.useContext(PokemonCacheContext)
+
+  if (!value) {
+    throw new Error('usePokemonCache must be use inside PokemonCacheProvider')
+  }
+
+  return value
+}
 
 function pokemonCacheReducer(state, action) {
   switch (action.type) {
@@ -36,9 +52,7 @@ function pokemonCacheReducer(state, action) {
 }
 
 function PokemonInfo({pokemonName}) {
-  // 💣 remove the useReducer here (or move it up to your PokemonCacheProvider)
-  const [cache, dispatch] = React.useReducer(pokemonCacheReducer, {})
-  // 🐨 get the cache and dispatch from useContext with PokemonCacheContext
+  const [cache, dispatch] = usePokemonCache()
 
   const {data: pokemon, status, error, run, setData} = useAsync()
 
@@ -69,8 +83,8 @@ function PokemonInfo({pokemonName}) {
 }
 
 function PreviousPokemon({onSelect}) {
-  // 🐨 get the cache from useContext with PokemonCacheContext
-  const cache = {}
+  const [cache, _dispatch] = usePokemonCache()
+
   return (
     <div>
       Previous Pokemon
@@ -91,20 +105,20 @@ function PreviousPokemon({onSelect}) {
 }
 
 function PokemonSection({onSelect, pokemonName}) {
-  // 🐨 wrap this in the PokemonCacheProvider so the PreviousPokemon
-  // and PokemonInfo components have access to that context.
   return (
-    <div style={{display: 'flex'}}>
-      <PreviousPokemon onSelect={onSelect} />
-      <div className="pokemon-info" style={{marginLeft: 10}}>
-        <PokemonErrorBoundary
-          onReset={() => onSelect('')}
-          resetKeys={[pokemonName]}
-        >
-          <PokemonInfo pokemonName={pokemonName} />
-        </PokemonErrorBoundary>
+    <PokemonCacheProvider>
+      <div style={{display: 'flex'}}>
+        <PreviousPokemon onSelect={onSelect} />
+        <div className="pokemon-info" style={{marginLeft: 10}}>
+          <PokemonErrorBoundary
+            onReset={() => onSelect('')}
+            resetKeys={[pokemonName]}
+          >
+            <PokemonInfo pokemonName={pokemonName} />
+          </PokemonErrorBoundary>
+        </div>
       </div>
-    </div>
+    </PokemonCacheProvider>
   )
 }
 
--- src/exercise/03.js
+++ src/exercise/03.js
@@ -5,22 +5,44 @@ import * as React from 'react'
 
 // 🐨 create your CountContext here with React.createContext
 
+const CountContext = React.createContext()
+
 // 🐨 create a CountProvider component here that does this:
 //   🐨 get the count state and setCount updater with React.useState
 //   🐨 create a `value` array with count and setCount
 //   🐨 return your context provider with the value assigned to that array and forward all the other props
 //   💰 more specifically, we need the children prop forwarded to the context provider
+function CountProvider(props) {
+  const [count, setCount] = React.useState(0)
+  const value = [count, setCount]
+
+  return (
+    <CountContext.Provider value={value} {...props}></CountContext.Provider>
+  )
+}
+
+function useCount() {
+  const value = React.useContext(CountContext)
+
+  if (!value) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+
+  return value
+}
 
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const count = 0
+  const [count] = useCount()
+
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const setCount = () => {}
+  const [_count, setCount] = React.useContext(CountContext)
   const increment = () => setCount(c => c + 1)
+
   return <button onClick={increment}>Increment count</button>
 }
 
@@ -31,8 +53,10 @@ function App() {
         🐨 wrap these two components in the CountProvider so they can access
         the CountContext value
       */}
-      <CountDisplay />
-      <Counter />
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
     </div>
   )
 }
